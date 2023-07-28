280fd79ab80f980852cdafd0947f3a29ff86e40c
Taras Oliinyk <oliinyk.tr@gmail.com>
2023-07-28 03:13:06 +0300

    Initial Commit

---
+++ News Explorer/News_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_ExplorerApp.swift
+//  News Explorer
+//
+//  Created by  User on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct News_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
